56d160b9d6d3c88af56eabbbe5a5a8e72e367726
farisubuntu <farisubuntu@gmail.com>
2023-11-08 23:13:09 +0300

    before fix

--- sections/script.js
+++ sections/script.js
@@ -1,4 +1,3 @@
-
 /**
  * state=2
  * 0=> all visible
@@ -33,17 +32,21 @@ function appendTenseTitles() {
    elem.setAttribute('class', 'title');
 
    var node = '';
-   if (k == 0) { node = document.createTextNode('الماضي'); }
-   if (k == 1) { node = document.createTextNode('الحاضر'); }
-   if (k == 2) { node = document.createTextNode('المستقبل'); }
+   if (k == 0) {
+    node = document.createTextNode('الماضي');
+   }
+   if (k == 1) {
+    node = document.createTextNode('الحاضر');
+   }
+   if (k == 2) {
+    node = document.createTextNode('المستقبل');
+   }
    elem.appendChild(node);
    tense.append(elem);
   }
 
  }
 }
-
-
 
 // Toggle button:
 
@@ -76,7 +79,9 @@ function toggle(col) {
 }
 function toggleCol(btn) {
 
- if (state < 0) { state = 2; }
+ if (state < 0) {
+  state = 2;
+ }
  toggle(state);
  state--;
 }
@@ -112,23 +117,105 @@ function changeFont() {
   document.querySelectorAll('.text .target').forEach(function (i) {
    i.style.fontFamily = "Noto Nastaliq Urdu, serif";
   });
- }
- else {
+ } else {
   selectedFont = 0;
   document.querySelectorAll('.text .target').forEach(function (i) {
    i.style.fontFamily = "Noto Naskh Arabic, serif";
   });
 
-
  }
 }
 
+var words_array = [];
+var images_array = [];
+var phrases_array = [];
 
-// to append scripts/quuick_selectors.js dynamically:
-function dynamicallyLoadScript() {
- var script = document.createElement("script");  // create a script DOM node
- script.src = "https://farisubuntu.github.io/mondly/scripts/quick_selectors.js" // set its src to the provided URL
+function getSectionVariables() {
+ //  select all words (one line container of (target,mother} =>
+ words_array = document.querySelectorAll('.list > .is-content-card-wrapper:first-child > .words > .word');
 
- document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+ //select all image urls for words, images_container[x].style.background-image=url("xxx")
+ var images_container = document.querySelectorAll('.phrase-image');
+ //  extract urls from images elements
+ var attrValue;
+ var start;
+ var end;
+ var urlValue;
 
+ images_container.forEach(
+  function (i) {
+   urlValue = i.style.backgroundImage;
+   urlValue = urlValue.replace('url("', '');
+   urlValue = urlValue.replace(')', '');
+   urlValue = urlValue.replace('"', '');
+   images_array.push(urlValue);
+  }
+ );
+ //------------------------
+ // select all phrases (one line container of (text elements {target,mother} ) =>
+ phrases_array = document.querySelectorAll('.phrase .text');
+}
+
+// template
+
+function extractMinifierData() {
+ getSectionVariables();
+ var top_container = `
+<div id="top-container">
+   <section id="words-container" class="section">`;
+ //    append words
+ var target;
+ var mother;
+ var image_url;
+ for (var i = 0; i < words_array.length; i++) {
+  target = words_array[i].querySelector('.target').innerText;
+  image_url = images_array[i];
+  mother = words_array[i].querySelector('.mother').innerText;
+  top_container += `
+        <article class="word-wrapper">
+          <div class="word-target">${target} </div>
+          <div class="word-mother">${mother}</div>
+          <div><img src=${image_url} alt="word-image" height="auto" width="30px"></div>
+    </article>
+    `;
+ }
+ top_container += `</section>
+    <section id="phrases-cotainer" class="section">`;
+ var phrase_target;
+ var phrase_mother;
+ for (var i = 0; i < phrases_array.length; i++) {
+  phrase_target = phrases_array[i].querySelector('.target').innerText;
+  phrase_mother = phrases_array[i].querySelector('.mother').innerText;
+  top_container += `
+          <article class="phrase">
+           <div class="phrase-target">${phrase_target}</div>
+           <div class="phrase-mother">${phrase_mother}</div>
+          </article>`;
+ }
+ top_container += `</section></div>`;
+
+ 
+ // add 'pre' tag to append top_container output string
+// var pre_tag=document.createElement('pre');
+// pre_tag.setAttribute('class','output');
+document.body.innerHTML='';
+// document.body.append(pre_tag);
+// pre_tag.textContent= top_container;
+// pre_tag.style.backgroundColor='whitesmoke';
+// pre_tag.style.color='green';
+
+document.body.style.padding='1.5rem';
+document.body.style.color='green';
+document.body.style.backgroundColor='whitesmoke';
+document.body.style.border='2px solid skyblue';
+document.body.style.direction='ltr';
+
+document.body.textContent=top_container;
+
+ 
+}
+
+
+function resetStyles() {
+document.body.style.all='unset';
 }
