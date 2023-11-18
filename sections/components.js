fe40e58e52bba17f81080df8e57b3c9d3ac2f383
farisubuntu <farisubuntu@gmail.com>
2023-11-18 17:42:00 +0300

    add components.js file

---
+++ sections/components.js
@@ -0,0 +1,23 @@
+// components:
+
+
+
+var floatButtons = `
+<button class="sticky-btn" onclick="changeFont();">تبديل الخط</button>
+<button id="toggle" class="sticky-btn" onclick="toggleCol(this);">إخفاء</button>
+<button id="home" class="sticky-btn"><a href="../index.html">الصفحة الرئيسية </a></button>
+<details class="sticky-btn">
+<summary>جداول</summary>
+<li><a href="../others/personal_pronouns_chart.html" target="_blank">ملخص الضمائر الشخصية</a></li>
+<li><a href="../others/verbs.html">الأفعال - الأزمنة البسيطة</a></li> 
+<li><a href="../others/important_vocabularies.html">كلمات مهمة</a></li>
+<li><a href="../mondly_dict/dict.html">قاموس ماندلي</a></li>
+</details>
+`;
+
+
+var buttonBar=`
+<div id="button-bar">
+<button class="feedback" onclick="toggleVerbs(this);">بدون الأفعال</button>
+</div>
+`;
--- sections/script.js
+++ sections/script.js
@@ -28,23 +28,23 @@ function appendTenseTitles() {
    var tense = verb_tenses[i].children[k];
 
    var elem = document.createElement('div');
-   
-   
-   
+
+
+
    var node = '';
    if (k == 0) {
-    node=`<img src="https://farisubuntu.github.io/mondly/sections/icons/past.png" />`;
+    node = `<img src="https://farisubuntu.github.io/mondly/sections/icons/past.png" />`;
    }
    if (k == 1) {
-    node=`<img src="https://farisubuntu.github.io/mondly/sections/icons/present.png" />`;
-    
+    node = `<img src="https://farisubuntu.github.io/mondly/sections/icons/present.png" />`;
+
    }
    if (k == 2) {
-    node=`<img src="https://farisubuntu.github.io/mondly/sections/icons/future.png" />`;
- 
+    node = `<img src="https://farisubuntu.github.io/mondly/sections/icons/future.png" />`;
+
    }
    elem.setAttribute('class', `conj tense-${k}`);
-   elem.innerHTML=node;
+   elem.innerHTML = node;
    tense.prepend(elem);
   }
 
@@ -92,22 +92,11 @@ function toggleCol(btn) {
 // create sticky buttons:
 function createStickyButtons() {
  console.log('createStickyButtons()...');
- var str = `
-   <button class="sticky-btn" onclick="changeFont();">تبديل الخط</button>
-   <button id="toggle" class="sticky-btn" onclick="toggleCol(this);">إخفاء</button>
-   <button id="home" class="sticky-btn"><a href="../index.html">الصفحة الرئيسية </a></button>
-   <details class="sticky-btn">
- <summary>جداول</summary>
- <li><a href="../others/personal_pronouns_chart.html" target="_blank">ملخص الضمائر الشخصية</a></li>
- <li><a href="../others/verbs.html">الأفعال - الأزمنة البسيطة</a></li> 
- <li><a href="../others/important_vocabularies.html">كلمات مهمة</a></li>
- <li><a href="../mondly_dict/dict.html">قاموس ماندلي</a></li>
-</details>
- `;
+
 
  var divBox = document.createElement('div');
  divBox.setAttribute('class', 'btns-wrapper');
- divBox.innerHTML = str;
+ divBox.innerHTML = floatButtons;
  document.body.prepend(divBox);
 
 }
@@ -159,87 +148,100 @@ function getSectionVariables() {
 }
 
 
- // template
+// template
 
- function getDictionaryData() {
-  getSectionVariables();  // this will fill global variable images_array,....
-  words_len = words_array.length
-  images_len = images_array.length
-  phrases_len = phrases_array.length
-  var top_container = `<div class="lesson-wrapper"><!-- ${window.location.pathname} -->
+function getDictionaryData() {
+ getSectionVariables();  // this will fill global variable images_array,....
+ words_len = words_array.length
+ images_len = images_array.length
+ phrases_len = phrases_array.length
+ var top_container = `<div class="lesson-wrapper"><!-- ${window.location.pathname} -->
  <header><p class="lesson-title">${window.location.pathname}</p></header>
  <section class="words-container">`;
 
-  //    append words
-  var target;
-  var mother;
-  var image_url;
-  for (var i = 0; i < words_array.length; i++) {
-   target = words_array[i].querySelector('.target').innerText;
-   image_url = images_array[i];
-   mother = words_array[i].querySelector('.mother').innerText;
-   top_container += `
+ //    append words
+ var target;
+ var mother;
+ var image_url;
+ for (var i = 0; i < words_array.length; i++) {
+  target = words_array[i].querySelector('.target').innerText;
+  image_url = images_array[i];
+  mother = words_array[i].querySelector('.mother').innerText;
+  top_container += `
         <article class="word-wrapper">
           <div class="word-target">${target}</div>
           <div class="word-mother">${mother}</div>`;
-   if (i < images_len) {
-    top_container += `<div class="image-wrapper"><img src='${image_url}' /"></div>
+  if (i < images_len) {
+   top_container += `<div class="image-wrapper"><img src='${image_url}' /"></div>
                  </article>`;
 
-   }
-   else {
-    top_container += `</article>`;
-   }
   }
-  top_container += `</section><section class="phrases-container">`;
-  var phrase_target;
-  var phrase_mother;
-  for (var i = 0; i < phrases_array.length; i++) {
-   phrase_target = phrases_array[i].querySelector('.target').innerText;
-   phrase_mother = phrases_array[i].querySelector('.mother').innerText;
-   top_container += `
+  else {
+   top_container += `</article>`;
+  }
+ }
+ top_container += `</section><section class="phrases-container">`;
+ var phrase_target;
+ var phrase_mother;
+ for (var i = 0; i < phrases_array.length; i++) {
+  phrase_target = phrases_array[i].querySelector('.target').innerText;
+  phrase_mother = phrases_array[i].querySelector('.mother').innerText;
+  top_container += `
           <article class="phrase-wrapper">
            <div class="phrase-target">${phrase_target}</div>
            <div class="phrase-mother">${phrase_mother}</div>
           </article>`;
-  }
-  top_container += `</section> </div><!-- end of lesson-wrapper --> `;
-
-
-  // add 'pre' tag to append top_container output string
-  // var pre_tag=document.createElement('pre');
-  // pre_tag.setAttribute('class','output');
-  document.body.innerHTML = '';
-  // document.body.append(pre_tag);
-  // pre_tag.textContent= top_container;
-  // pre_tag.style.backgroundColor='whitesmoke';
-  // pre_tag.style.color='green';
-
-  document.body.style.padding = '1.5rem';
-  document.body.style.color = 'green';
-  document.body.style.backgroundColor = 'whitesmoke';
-  document.body.style.border = '2px solid skyblue';
-  document.body.style.direction = 'ltr';
-
-  document.body.textContent = top_container;
-
-
  }
+ top_container += `</section> </div><!-- end of lesson-wrapper --> `;
 
-function appendClasses(){
+ document.body.innerHTML = '';
+
+ document.body.style.padding = '1.5rem';
+ document.body.style.color = 'green';
+ document.body.style.backgroundColor = 'whitesmoke';
+ document.body.style.border = '2px solid skyblue';
+ document.body.style.direction = 'ltr';
+
+ document.body.textContent = top_container;
+
+
+}
+
+function appendClasses() {
  // append 'snow' class to .is-content-card-wraper
- var elements=document.querySelectorAll('.is-content-card-wrapper:first-child');
- console.log('elements.lenght= ',elements.length);
+ var elements = document.querySelectorAll('.is-content-card-wrapper:first-child');
+ console.log('elements.lenght= ', elements.length);
 
- elements[0].className="snow";
- elements[3].className="snow";
- elements[5].className="snow";
+ elements[0].className = "snow";
+ elements[3].className = "snow";
+ elements[5].className = "snow";
 
+}
+
+
+
+function embedVerbsShow() {
+ var buttonsContainer = document.createElement('div');
+ buttonsContainer.innerHTML = buttonBar;
+ document.body.append(buttonsContainer);
+}
+function printHello() {
+ console.log("HELLLLLLLLLLLLLLLO");
+}
+
+var verbs = 1; // default=displayed, 0 hidden
+function toggleVerbs(sender) {
+ console.log('toggleVerbs() invoked');
+ if (verbs == 1) {
+  document.querySelectorAll('.verbs').forEach(function name(e) {
+   e.style.display = 'none';
+  });
+  verbs = 0;
  }
-
-
-
- function embedVerbsShow(){
-  var lessonHeader=document.querySelector('header.page-header');
-  
+ else {
+  document.querySelectorAll('.verbs').forEach(function name(e) {
+   e.style.display = 'initial';
+  });
+  verbs = 1;
  }
+}
