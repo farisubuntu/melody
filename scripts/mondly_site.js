9192e3d3834cf0d37feb368f015f16aebf419326
farisubuntu <farisubuntu@gmail.com>
2023-11-27 14:34:34 +0300

    add 3 new categories

--- scripts/mondly_site.js
+++ scripts/mondly_site.js
@@ -11,3 +11,42 @@ function get_lessons_titles() {
   html = html.replaceAll("\n", "");
   return html;
 }
+
+function getTopHtml(){
+  var topHtml=`
+  <!DOCTYPE html>
+<html lang="en" dir="rtl">
+
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <link rel="stylesheet" href="../w3pro.css">
+ <link rel="stylesheet" href="main.css">
+ <title>?????</title>
+</head>
+
+<body>
+
+ <div id="media-bar"></div>
+
+ <header class="page-header">
+  <h1 class="button-85" role="button">?????</h1>
+  <div class="video-wrapper">
+   <video class="op-player op-player__media" controls>
+    <source src="#" type="audio/mpeg" />
+   </video>
+  </div>
+ </header>
+
+
+  `;
+return topHtml;
+}
+
+
+
+
+function getPageTemplate(){
+  var htmlTemplate=getTopHtml() + get_lessons_titles(); 
+  return htmlTemplate;
+}
